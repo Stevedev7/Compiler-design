b26e3f128502403d851bc16ded3b7b94e77e2439
Steve Pinto <pinto_steve@yahoo.com>
2020-02-27 18:24:21 +0530

    First commit

---
+++ test.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(){
+    //Never Delete this File... This File is an actual Wonder!
+    //If you dont believe me then See it for yourself
+    //declare a variable
+    int a;
+    /*LOL
+    LOL
+    LOL
+    LOL
+    LOL
+    F
+    F
+    F
+    F
+    F
+        f
+        f
+        f
+        f
+            f
+                f
+            f
+    F
+    F
+    */
+   //print some lines
+   printf("Say Lol");
+
+   //print some more stuff
+
+   printf("Lets play the circle game");
+
+   /*This is a huge comment
+   Which is added just for the sake of testing the lex file
+   Adding some more lines to the multiline comment
+   Fs in the chat for our fallen brother
+   let me end the multi-line comment here*/
+   //because this is getting a little too lengthy
+   //let me compute something so that i give a purpose to this huge-ass c program
+   a = 69;
+   /*let me print the value of a
+   because... Why not?
+   i just wanted to print the number 69
+   this number is a beauty
+   just like its elder brother
+   which im gonna print out in the later on lines
+   Sayonara */
+   printf("This line is to print the number %d\n",a);
+   //So yeah i was talking about another beautiful number 420
+   //There is no perfect number which is not 420
+   //Because it is perfect
+   //Well i forgot to mention there is another number which surpasses all numbers
+   //I'll print it out in the later lines
+   //So just sit back and enjoy the show
+   a = 420;
+   printf("Printing %d\n", a);
+   /*So yeah im dragging this program intentionally so that i get more number of comments
+   Because WHOLE POINT OF THIS GOD FORSAKEN PROGRAM IS TO COUNT THE NUMBER OF GODDAMN COMMENT LINES
+   */
+  //Let me stop talking gibberish and get into the best part of this program
+  //Let me print the most perfect number in the whole numbering system
+  //Brace yourselfs my degenerate brothers and sisters
+  //Here it goes
+  a = 69420;
+  //Well The person who was born on April 20th 1969 would have been a cool nigga because his birthdate would've been 42069!!!
+  //Aplause!!!!
+  //Let's cut the shit and see the number and bless our eyes
+  //We've all been waiting for this moment
+  //Here it goes... Brace yourselves
+
+  printf("The most perfect number, Ladies and Gentlemen!! -> %d", a);
+
+  //Bruh!
+  return 0;
+
+}
